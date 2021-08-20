117c16ed6732e1d7cdcb1723288a4656fd6b633d
zn <1244533983@qq.com>
2021-08-20 15:28:08 +0800

    server

---
+++ build/run.js
@@ -0,0 +1,14 @@
+const {spawn} = require('child_process');
+const path = require('path')
+const filePath = {
+    vue2: path.resolve(__dirname, '../vue2'),
+    vue3: path.resolve(__dirname, '../vue3'),
+    react17: path.resolve(__dirname, '../react17')
+}
+
+const runChild = () => {
+    Object.values(filePath).forEach(item => {
+        spawn(`cd ${item} && npm run dev`, {stdio: 'inherit',shell: true})
+    })
+}
+runChild()
